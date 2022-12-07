d75e5ce66fe5617f4862d4b8aa7a9a17c8875766
Moenni <nicolasmoen@live.com>
2022-12-07 11:48:25 -0300

    agrego carpeta 13 y 14

---
+++ Clase 14/JS/script.js
@@ -0,0 +1,27 @@
+let saludo= "Hola Mundo desde el archivo JS";
+console.log( saludo);
+
+let edad = parseInt(prompt("Ingrese su edad")); 
+
+if(edad>=18){ 
+
+console.log("Es mayor de edad"); 
+
+}else{ 
+
+console.log("Es menor de edad"); 
+
+} 
+let miObjeto = { 
+
+    mensaje: "Mensaje de texto", 
+    
+    utilidad: "prueba de JS" 
+    
+    }; 
+    
+    let miArray = [ "Primer mensaje del array", "Segundo mensaje del array", "Tercer mensaje del array"] 
+    
+    console.table(miObjeto); 
+    
+    console.table(miArray); 
