170e1fb7029c874131709880c5b34a6faae73290
Moenni <nicolasmoen@live.com>
2022-12-11 16:13:18 -0300

    probando funcionalidad del teclado y mouse

--- Clase 16/elementos y atributos/codigo.js
+++ Clase 16/elementos y atributos/codigo.js
@@ -1,13 +1,24 @@
 //1- Agregar a la tarjeta, el atributo "class" con el valor "card"
-window.addEventListener("load", function(){
- let parrafoButton = document.querySelector(".parrafo")
+/*cambiar de color los parrafos , sale alerta para elegir */
+window.addEventListener("load",function(){
+    let botones = document.querySelectorAll(".w3-button")
 
- parrafoButton.addEventListener("click", function(){
-    alert("tocaste el boton de parrafo")
- })
+    for(let i =0;i<botones.length;i++){
+        botones[i].addEventListener("click",function(){
+          this.style.color = prompt("choose your color"); 
+    })
+    }
+})
+/*al presionar una tecla salta una alerta */
+window.addEventListener("keypress",function(e){
+  if(e.key=="Enter"){
+    alert("Ey! que tocas enter?");
+  }
+    /*alert(1);*/
 })
 
 
+/* al hacer clic en titulo , sale una alerta */
 window.addEventListener("load",function(){
     let tituloFeo= document.querySelector(".titulo-feo")
 
@@ -15,7 +26,7 @@ window.addEventListener("load",function(){
         alert("Has presionado el botón de Titulo")
     })
 })
-
+/*al hacer click en el logo sale una alerta */
 window.addEventListener("load",function(){
     let logoYoutube = document.querySelector(".logo")
     
