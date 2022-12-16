bee4f178638969624e9afdbd6f1bebc5edfae736
Moenni <nicolasmoen@live.com>
2022-12-16 13:25:34 -0300

    modifico index y rest

--- Clase 20/js/rest.js
+++ Clase 20/js/rest.js
@@ -1,12 +1,32 @@
-fetch("https://restcountries.com/v3.1/all")
+/*
+fetch("https://apis.datos.gob.ar/georef/api/provincias")
 .then(function(res) {
     return res.json();
 })
-.then(function(paises) {
-    paises.forEach(pais => {
-       console.log(pais.name);
+.then(function(datos) {
+    let inputSelect = document.querySelector("#provincia");
+
+    datos.provincias.forEach((provincia) => {
+        console.log(provincia);
+       inputSelect.innerHTML+= `<option value="${ provincia.id}">${ provincia.nombre}</option>`;
     });
-    console.log(paises);
-}).catch(function(err) {
+  })
+  .catch(function(err) {
+    console.error(err);r;
+});
+*/
+fetch("https://api.giphy.com/v1/gifs/search?api_key=P1Kb0LGaxh0eZeg5P8Toa1MlnPnXmmn8&q=lord+of+the+rings&limit=25&offset=0&rating=g&lang=en")
+.then(function(res) {
+    return res.json();
+})
+.then(function(datos) {
+    let listadoGifs = document.querySelector(".listado-gifs");
+
+    datos.data.forEach((gif) => {
+        listadoGifs.innerHTML+= `<img src="${ gif.images.original.url}">`;
+  
+    });
+  })
+  .catch(function(err) {
     console.error(err);r;
 });
