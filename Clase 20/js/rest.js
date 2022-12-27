9782749aeec150e2e613c278934f2f466a121575
Moenni <nicolasmoen@live.com>
2022-12-27 18:27:16 -0300

    modifico archivo rest carpeta 20

--- Clase 20/js/rest.js
+++ Clase 20/js/rest.js
@@ -15,6 +15,7 @@ fetch("https://apis.datos.gob.ar/georef/api/provincias")
     console.error(err);r;
 });
 */
+/*
 fetch("https://api.giphy.com/v1/gifs/search?api_key=P1Kb0LGaxh0eZeg5P8Toa1MlnPnXmmn8&q=lord+of+the+rings&limit=25&offset=0&rating=g&lang=en")
 .then(function(res) {
     return res.json();
@@ -30,3 +31,40 @@ fetch("https://api.giphy.com/v1/gifs/search?api_key=P1Kb0LGaxh0eZeg5P8Toa1MlnPnX
   .catch(function(err) {
     console.error(err);r;
 });
+*/
+/*
+//METODO GET
+fetch("https://reqres.in/api/users").then(function(response){
+  return response.json();
+}).then(function(users){
+    console.log(users.data);
+    users.data.forEach((user)=>{
+        console.log(user.first_name+" "+user.last_name);
+    })
+});
+*/
+let inputName= document.querySelector("#name");
+let inputJob= document.querySelector("#job");
+let formulario= document.querySelector("#formulario");
+
+formulario.addEventListener("submit",function(event){
+    event.preventDefault();
+
+
+//METODO POST
+fetch("https://reqres.in/api/users",{
+    method:"POST",
+    headers:{
+        contentType:"application/json",
+    },
+    body:JSON.stringify({
+        name:inputName.value ,
+        job: inputJob.value,
+    })
+}).then(function(response){
+   return response.json();
+    })
+    .then(function(user){
+        console.log(user);
+});
+})
